b57a1e666a5496efb5819d0e1ab3b27804805161
Malik Hamilton-Edwards <mhamilton-edwards@myseneca.ca>
2025-02-13 17:01:30 -0500

    Individual message or note.

---
+++ lab5a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Author ID: mhamilton-edwards
+
+def read_file_string(file_name):
+    """
+    Takes file_name as a string and returns its entire contents as a string.
+    """
+    with open(file_name, 'r') as file:
+        return file.read()
+
+def read_file_list(file_name):
+    """
+    Takes file_name as a string and returns its entire contents as a list of lines
+    without new-line characters.
+    """
+    with open(file_name, 'r') as file:
+        return [line.strip() for line in file.readlines()]
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
